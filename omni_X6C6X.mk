73859be7adc1c860f9850476a0a0bf126d1028ca
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-04-09 12:58:37 +0200

    X6C6X: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: lenovo Model: Lenovo TB-X6C6X Script version: 3.0.0

---
+++ omni_X6C6X.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from X6C6X device
+$(call inherit-product, device/lenovo/X6C6X/device.mk)
+
+PRODUCT_DEVICE := X6C6X
+PRODUCT_NAME := omni_X6C6X
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo TB-X6C6X
+PRODUCT_MANUFACTURER := lenovo
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo-rvo3
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_P98928AA1-userdebug 11 RP1A.200720.011 b8766p164bspP17 test-keys"
+
+BUILD_FINGERPRINT := Lenovo/LenovoTB-X6C6X/X6C6X:11/RP1A.200720.011/S092_210902_ROW:userdebug/test-keys
