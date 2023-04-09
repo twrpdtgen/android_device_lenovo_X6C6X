877a9f2ef7d47ad4821d7af12f61646874d588bb
opsonc <58789337+opsonc@users.noreply.github.com>
2023-04-09 14:04:22 +0300

    Update omni_X6C6X.mk

--- omni_X6C6X.mk
+++ omni_X6C6X.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X6C6X device
 $(call inherit-product, device/lenovo/X6C6X/device.mk)
 
 PRODUCT_DEVICE := X6C6X
-PRODUCT_NAME := omni_X6C6X
+PRODUCT_NAME := twrp_X6C6X
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo TB-X6C6X
 PRODUCT_MANUFACTURER := lenovo
