14b7b640ee0c13a20db98fee6348a46daa23bb6b
opsonc <58789337+opsonc@users.noreply.github.com>
2023-04-09 14:05:02 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_X6C6X-user
-add_lunch_combo omni_X6C6X-userdebug
-add_lunch_combo omni_X6C6X-eng
+add_lunch_combo twrp_X6C6X-user
+add_lunch_combo twrp_X6C6X-userdebug
+add_lunch_combo twrp_X6C6X-eng
